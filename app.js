04822d44a0b0c9b23c3c8c749dc0b6436fcfa6a5
ahmetemreakin <akinahmetemre@gmail.com>
2025-06-30 16:22:10 +0300

    asd

---
+++ app.js
@@ -0,0 +1,13 @@
+function takedata(){
+  
+  const name=document.querySelector("#name");
+const email=document.getElementById('email').value;
+const selectedValue = document.getElementById('position').value;
+console.log("Seçilen pozisyon:", selectedValue);
+console.log("Seçilen name:", name.value);
+console.log("Seçilen email:", email);
+alert("hesabınız oluşturulmuştur.")
+
+console.log(name.value)
+  
+}
